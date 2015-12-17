cfa366f0c4df6a0084df96b247d4e912c002284c
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2015-12-17 23:12:10 +0530

    Added mocha

--- templates/common/lib/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var s = function s() {
-	console.log('s');
-};
---
+++ test/generator.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('It should run',function(){
+	it('should return true',function(){
+		assert.equal(true,true);
+	});
+})
